b1064bfdf1b170434b028f468d83cdc94ff92544
Christie Lynam <christielynam@gmail.com>
2017-08-30 15:52:15 -0600

    add key file to git ignore

--- src/reducers/movies-reducer.js
+++ src/reducers/movies-reducer.js
@@ -1,6 +1,7 @@
 const movies = (state = [], action) => {
   switch (action.type) {
     case 'ADD_MOVIES':
+    console.log('add movie action called');
       return [...state, ...action.data]
     default:
       return state
---
+++ utils/key.js
@@ -0,0 +1,3 @@
+ const key = '0b1096e689aa8acb28ccef63b3a935c0';
+
+ module.exports = key;
