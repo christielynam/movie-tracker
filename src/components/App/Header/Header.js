e4464493b396df632028e50b13042d4de359bcee
Christie Lynam <christielynam@gmail.com>
2017-08-29 20:46:51 -0600

    partial build out of components and containers

---
+++ src/components/App/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header>
+      <h1 className='title'>Movie Tracker</h1>
+      <UserControls />
+
+    </header>
+  )
+}
+
+export default Header;
--- src/containers/body-container.js
+++ src/containers/body-container.js
@@ -4,4 +4,20 @@ import FullMovieDetail from '../components/App/Body/FullMovieDetail';
 import MovieDirectory from '../components/App/Body/MovieDirectory';
 import MovieCard from '../components/App/Body/MovieDirectory/MovieCard';
 
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const dispatchStateToProps = (dispatch) => {
+  return {
+
+  }
+}
+
 export default connect(mapStateToProps, mapDispatchToProps)([Body, FullMovieDetail, MovieDirectory, MovieCard]);
+
+//dont think we need a body container
+//we will just need containers for any component that needs access to the store ie... movieDirectory, FullMovieDetail, MovieCard
