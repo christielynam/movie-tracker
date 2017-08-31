0e3beab105fbb97e16b6a877e67d26a230a9d990
Nik Bornemeier <NikolausRB@gmail.com>
2017-08-31 10:58:48 -0600

    add url for images

--- src/components/App/Body/MovieDirectory/MovieCard/MovieCard.js
+++ src/components/App/Body/MovieDirectory/MovieCard/MovieCard.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const MovieCard = ({ movie }) => {
   return(
-    <h1>{ movie.title }</h1>
+    <div>
+      <h1>{ movie.title }</h1>
+      <img className='movie-poster' src={`https://image.tmdb.org/t/p/w500${movie.posterImg}`} />
+    </div>
   )
 }
 
