46615a9706967f2c2a5e736bdb498f6a0dda7727
Nick Svetnicka <endlesshypnosis@gmail.com>
2017-09-04 12:05:47 -0600

    fixed some of the syntax for const function

--- src/components/App/Body/FullMovieDetail/FullMovieDetail.js
+++ src/components/App/Body/FullMovieDetail/FullMovieDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const FullMovieDetail(movie) {
+const FullMovieDetail = (props) => {
 
     const favClass = props.movie.isFavorited ? "favorite-movie-btn favorited-movie-active" : "favorite-movie-btn"
 
     return(
         <div>
-        <h3>${movie.title}</h3>
+        <h3>${props.movie.title}</h3>
         <button className={favClass}
                 type="button"
                 onClick={(e) => {
@@ -15,9 +15,11 @@ const FullMovieDetail(movie) {
                 }}
                 ></button>
         <img className='movie-poster' src={`https://image.tmdb.org/t/p/w500${props.movie.posterImg}`} />
-        <p>${movie.releaseDate}</p>
-        <p>${movie.voteAverage}</p>
-        <p>${movie.description}</p>
+        <p>${props.movie.releaseDate}</p>
+        <p>${props.movie.voteAverage}</p>
+        <p>${props.movie.description}</p>
         </div>
     )
 }
+
+export default FullMovieDetail;
